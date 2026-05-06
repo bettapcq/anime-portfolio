a4fdb9215d71ce84f68d77b68283d39d2e149399
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-05-06 20:02:13 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+function App() {
+  return (
+    <main>
+      <Container>Anime Portfolio</Container>
+    </main>
+  );
+}
+
+export default App;
